7175cc03f00e80b610c1e91184f08f11da3f1c95
Andrea Tresoldi <andreatresoldi@MacBook-Pro-di-Andrea.local>
2019-04-11 17:09:16 +0200

    update

--- src/main/java/it/polimi/model/Map.java
+++ src/main/java/it/polimi/model/Map.java
@@ -6,7 +6,7 @@ public class Map {
 
     public void getSquare (int r, int c){
 
-        // TODO metodo che ritorna il riferimento ad una sola square indicizzata da r e c
+        // TODO metodo che ritorna il riferimento di una sola square indicizzata da r e c
         // il valore di ritorno non sarà void ma Square
 
 
