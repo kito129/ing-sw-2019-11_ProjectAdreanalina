4c19f5bf83c9111caf066ad7c4dbe676e4399193
simone zani <10502938@polimi.it>
2019-05-03 13:15:40 +0200

    add printPlayer

---
+++ src/main/java/it/polimi/view/cli/PrintPlayer.java
@@ -0,0 +1,36 @@
+package it.polimi.view.cli;
+
+import it.polimi.model.Player;
+
+import java.io.Serializable;
+
+public class PrintPlayer implements Serializable {
+
+    int id;
+    String name;
+    int row;
+    int column;
+    int score;
+    boolean alive;
+
+    /**
+     * Print Player's attributes.
+     */
+    public void print(Player player){
+
+        id = player.getId();
+        name = player.getName();
+        row = player.getRow();
+        column = player.getColumn();
+        score = player.getScore();
+        alive = player.isAlive();
+
+        System.out.println("ID: " +id);
+        System.out.println("NAME: " +name);
+        System.out.println("ROW: " +row);
+        System.out.println("COLUMN: " +column);
+        System.out.println("SCORE: " +score);
+        System.out.println("ALIVE: " +alive);
+
+    }
+}
