c646bbe3a0ce4aa7c8db35ec1390e5f484a2ebb0
simone zani <10502938@polimi.it>
2019-05-07 17:46:10 +0200

    printMap

--- src/main/java/it/polimi/view/cli/PrintMap.java
+++ src/main/java/it/polimi/view/cli/PrintMap.java
@@ -1,8 +1,12 @@
 package it.polimi.view.cli;
 
+import it.polimi.model.Square;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class PrintMap implements Serializable {
 
+    public static void print(ArrayList<Square> squares) {
 
 }
