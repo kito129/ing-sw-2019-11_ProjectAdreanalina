81e7650ccb551ef3e5f342e401b3de77b8d84bde
chito <marco3.selva@mail.polimi.it>
2019-05-12 22:06:14 +0200

    12/05

--- src/main/java/it/polimi/model/ActionModel.java
+++ src/main/java/it/polimi/model/ActionModel.java
@@ -17,23 +17,43 @@ import it.polimi.model.PowerUp.Teleporter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * The type Action model.
+ */
 public class ActionModel {
     
     private GameModel gameModel;
     private int action=0;
+    
     Map map=gameModel.getMap();
     Player actualPlayer=gameModel.getActualPlayer();
     
+    /**
+     * Gets action number.
+     *
+     * @return the action number
+     */
     public int getAction () {
         
         return action;
     }
     
+    /**
+     * Gets game model.
+     *
+     * @return the game model
+     */
     public GameModel getGameModel () {
         
         return gameModel;
     }
     
+    /**
+     * Run Action.
+     *
+     * @param targetSquare the target square to move
+     * @throws RunActionMaxDistLimitException the run action max dist limit exception
+     */
     public void runActionModel (Square targetSquare) throws RunActionMaxDistLimitException {
         
         if(map.distance(map.findPlayer(actualPlayer),targetSquare)<4){
@@ -47,6 +67,14 @@ public class ActionModel {
         }
     }
     
+    /**
+     * Grab Action.
+     *
+     * @param targetSquare the target square where grab
+     * @param weaponIndex  the weapon index position in generation square
+     * @throws CatchActionMaxDistLimitException the catch action max dist limit exception
+     * @throws CatchActionFullObjException      the catch action full obj exception
+     */
     public void grabActionModel (Square targetSquare, int weaponIndex) throws CatchActionMaxDistLimitException, CatchActionFullObjException {
         
         //adrenalinic distance
@@ -59,7 +87,6 @@ public class ActionModel {
             
             maxDist = 2;
         }
-        
         if(map.distance(map.findPlayer(actualPlayer),targetSquare)<maxDist) {
             
             map.movePlayer(actualPlayer,targetSquare);
@@ -72,6 +99,7 @@ public class ActionModel {
                 actualPlayer.getPlayerBoard().addWeapon(((GenerationSquare) map.findPlayer(actualPlayer)).catchWeapon(weaponIndex));
                 action++;
             }else {
+                
                 throw new CatchActionFullObjException();
             }
         } else {
@@ -80,7 +108,12 @@ public class ActionModel {
         }
     }
     
-    public boolean checkTurn(){
+    /**
+     * Check the number in the turn.
+     *
+     * @return true if can do action, else otherwise
+     */
+    public boolean checkActionCount (){
         if(action==1){
             
             gameModel.setState(State.ACTION1);
@@ -112,6 +145,16 @@ public class ActionModel {
         return null;
     }
     
+    /**
+     * Use power up Newton.
+     *
+     * @param newton       the newton powerUp
+     * @param targetPlayer the target player
+     * @param targetSquare the target square
+     * @throws NoPowerUpAvaible   No power up avaible
+     * @throws NotInSameDirection Not in same direction
+     * @throws NotValidDistance   Not valid distance
+     */
     public void usePowerUpNewton(Newton newton,Player targetPlayer, Square targetSquare) throws NoPowerUpAvaible, NotInSameDirection, NotValidDistance {
         
         gameModel.setState(State.POWERUP);
@@ -120,6 +163,13 @@ public class ActionModel {
         
     }
     
+    /**
+     * Use power up Teleporter.
+     *
+     * @param teleporter   the teleporter PowerUp
+     * @param targetSquare the target square
+     * @throws NoPowerUpAvaible the no power up avaible
+     */
     public void usePowerUpTeleporter(Teleporter teleporter, Square targetSquare) throws NoPowerUpAvaible {
     
         gameModel.setState(State.POWERUP);
@@ -128,32 +178,51 @@ public class ActionModel {
         
     }
     
-    public void usePowerUpTargetingScope(TargetingScope tagertingScope,Player targetPlayer) throws NoPowerUpAvaible {
+    /**
+     * Use power up Targeting Scope.
+     *
+     * @param targetingScope the targeting scope PowerUp
+     * @param targetPlayer   the target player
+     * @throws NoPowerUpAvaible NO power up available
+     */
+    public void usePowerUpTargetingScope(TargetingScope targetingScope,Player targetPlayer) throws NoPowerUpAvaible {
     
         gameModel.setState(State.POWERUP);
-        TargetingScope tagertingScope1=(TargetingScope) getPowerUp(tagertingScope);
+        TargetingScope tagertingScope1=(TargetingScope) getPowerUp(targetingScope);
         tagertingScope1.effect(gameModel.getActualPlayer(),targetPlayer);
         
     }
     
+    /**
+     * Use power up Tag Back Grenade.
+     *
+     * @param tagBackGrenade the tag back grenade powerUp
+     * @param targetPlayer   the target player
+     * @throws NoPowerUpAvaible No power up avaible
+     * @throws NotVisibleTarget Not visible target
+     */
     public void usePowerUpTagBackGrenade(TagBackGrenade tagBackGrenade, Player targetPlayer) throws NoPowerUpAvaible, NotVisibleTarget {
-        
+    
         gameModel.setState(State.POWERUP);
-        TagBackGrenade tagBackGrenade1=(TagBackGrenade) getPowerUp(tagBackGrenade);
-        tagBackGrenade1.effect(gameModel.getMap(),gameModel.getActualPlayer(),targetPlayer);
-        
+        TagBackGrenade tagBackGrenade1 = (TagBackGrenade) getPowerUp(tagBackGrenade);
+        tagBackGrenade1.effect(gameModel.getMap(), gameModel.getActualPlayer(), targetPlayer);
     }
     
-    public void rechargeWeapon(WeaponCard weapon,Player player, ArrayList<PowerUpCard> powerUpCardsToAmmo){
-        
-        }
-        
+    /**
+     * Respawn player.
+     *
+     * @param player           the player to respawn
+     * @param generationSquare the generation square
+     * @param powerUpCard      the power up card to add to player
+     */
     public void respawnPlayer(Player player, Square generationSquare, PowerUpCard powerUpCard){
         
+        player.getPlayerBoard().resetDamage();
         player.setAlive(true);
         gameModel.getMap().addPlayerOnSquare(generationSquare,player);
         player.getPlayerBoard().addPowerUp(powerUpCard);
     }
+    
     
     public void scoringPlayerBoard(Player player) {
     
@@ -163,7 +232,7 @@ public class ActionModel {
         for (Player a : gameModel.getPlayers()) {
             colorDamageHashMap.put(a.getColor(), player.getPlayerBoard().colorOccurrenceInDamages(a.getColor()));
         }
-        //TODO AVANTI DA QUI 6/05
+        
         //first blood
         playerBoard.getDamages();
         }
