50ad0d5036c2c555f94d7dcab051c534459003b3
chito <marco3.selva@mail.polimi.it>
2019-06-10 10:24:27 +0200

    add weapon state

---
+++ src/main/java/it/polimi/model/WeaponState.java
@@ -0,0 +1,7 @@
+package it.polimi.model;
+
+public enum WeaponState {
+    Cyberblade,Electroscythe,Flamethrower,Furnace,GrenadeLauncher,Heatseeker,Hellion,LockRifle,MachineGun,PlasmaGun,PowerGlove,Railgun,
+    RocketLauncher,Shockwave,Shotgun,Sledgehammer,Thor,TractorBeam,VortexCannon,Whisper,Zx2
+    
+}
