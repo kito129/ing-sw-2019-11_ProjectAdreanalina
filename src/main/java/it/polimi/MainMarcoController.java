89a0b56024c1baaaa704dc10b0e629ad2b861ef4
simone zani <10502938@polimi.it>
2019-06-18 17:10:37 +0200

    update

--- src/main/java/it/polimi/MainMarcoController.java
+++ src/main/java/it/polimi/MainMarcoController.java
@@ -5,6 +5,7 @@ import it.polimi.controller.GameController;
 import it.polimi.model.*;
 import it.polimi.model.Exception.MapException;
 import it.polimi.model.Weapon.LockRifle;
+import it.polimi.view.cli.PrintMap;
 import it.polimi.view.cli.ViewCLI;
 
 import java.rmi.RemoteException;
@@ -17,59 +18,59 @@ public class MainMarcoController {
     
     public static void main (String args[]) throws RemoteException {
 
-        
+
     
         System.out.println("creo killshot track" );
         ArrayList<Player> players = new ArrayList<>();
-        
+
         System.out.println("creo player\n");
         Player player1 = new Player(1, "andrea", EnumColorPlayer.YELLOW);
         Player player2 = new Player(2, "marco", EnumColorPlayer.BLU);
         Player player3 = new Player(3, "simone", EnumColorPlayer.GREEN);
         Player player4 = new Player(4, "niko", EnumColorPlayer.PINK);
         Player player5 = new Player(5, "teo", EnumColorPlayer.GREY);
-        
+
         for (Player p : players) {
-        
+
             p.stampa();
         }
-    
+
         System.out.println("creo mappa");
-        
-        
+
+
         //aggiungo player
         players.add(player1);
         players.add(player2);
         players.add(player3);
         players.add(player4);
         players.add(player5);
-        
+
         //creo oggettti di modello
         System.out.println("creo oggetti modello");
         GameModel gameModel = new GameModel(); //setto lo stato in select run
         gameModel.getPlayers(true).addAll(players);
-        
+
         gameModel.setActualPlayer(player1);
         ActionModel actionModel = new ActionModel(gameModel);
         ActionController actionController = new ActionController();
         GameController gameController = new GameController();
-        
-    
+
+
         //metto oggetti in mappa
         System.out.println("refresh mappa");
         //actionModel.refreshMapAmmoCard();
         //actionModel.refreshMapWeaponCard();
-        
+
         Map map = gameModel.getMap();
-    
+
         System.out.println("Stampa finale");
         map.print();
-        
-        
+
+
         //obesrver
-        
-        
-        
+
+
+
         //piazza i player in mappa
         try {
             map.addPlayerOnSquare(map.getSquare(0, 1), player1);
@@ -96,20 +97,19 @@ public class MainMarcoController {
         } catch (MapException e) {
             System.out.println("error");
         }
-        
+
         for (Player p : players) {
-            
+
             p.stampa();
         }
-    
+
         ViewCLI viewCLI = new ViewCLI(gameController);
         gameModel.addObserver(viewCLI);
         map.print();
-        viewCLI.printMap();
-        
-       
+        PrintMap.printMap(gameModel.getMap());
 
-        
+
+
        /*
         //prova run
     
--- src/main/java/it/polimi/view/gui/StartController.java
+++ src/main/java/it/polimi/view/gui/StartController.java
@@ -1,4 +1,154 @@
 package it.polimi.view.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StartController {
+
+    @FXML
+    protected AnchorPane container;
+
+    @FXML
+    protected Button startButton;
+
+    @FXML
+    protected TextField username;
+
+    @FXML
+    private Label error, lobby, message;
+
+    private int state = 0;
+    private boolean wrongIP = false;
+    private ViewGUI viewGUI;
+
+
+    /**
+     * sets if the ip address inserted is wrong
+     * @param wrongIP the wrongIP
+     */
+    public void setWrongIP(boolean wrongIP) {
+
+        this.wrongIP = wrongIP;
+    }
+
+    /**
+     * sets the viewGUI reference
+     * @param viewGUI the viewGUI reference
+     */
+    void setViewGUI(ViewGUI viewGUI){
+
+        this.viewGUI = viewGUI;
+    }
+
+    /**
+     * sets the client username
+     * @param s username
+     */
+    private void setUser(String s){
+
+        viewGUI.setUser(s);
+        container.getChildren().removeAll(startButton, username, error);
+        message.setVisible(true);
+        message.setText("YOU HAVE BEEN ADDED TO THIS GAME!\nIT WILL START IN A FEW MOMENTS");
+    }
+
+    /**
+     * hides all if the game is already started
+     */
+    private void gameStarted(){
+
+        container.getChildren().removeAll(message, lobby, startButton, username);
+        error.setVisible(true);
+        error.setText("OPS! THE GAME IS ALREADY STARTED!\nCOME BACK LATER!");
+    }
+
+    /**
+     * enables to insert the username
+     */
+    private void insertUsername(){
+
+        startButton.setVisible(false);
+        message.setVisible(false);
+        username.clear();
+        username.setVisible(true);
+        username.setPromptText("username");
+    }
+
+    /**
+     * initializes the first GUI screen
+     */
+    void init(){
+
+        error.managedProperty().bind(error.visibleProperty());
+        username.managedProperty().bind(username.visibleProperty());
+        lobby.managedProperty().bind(lobby.visibleProperty());
+        startButton.managedProperty().bind(startButton.visibleProperty());
+    }
+
+    /**
+     * show a text in the lobby
+     */
+    void printLobby(){
+
+        lobby.setText("GAMERS IN THE LOBBY:\n");
+    }
+
+    /**
+     * show the player username
+     * @param s player username
+     */
+    void addPrint(String s){
+
+        lobby.setText(lobby.getText() + s + "\n");
+    }
+
+    /**
+     * show the string error
+     * @param s string error
+     */
+    void printError(String s){
+
+        container.getChildren().removeAll(startButton, message, lobby, username);
+        error.setVisible(true);
+        error.setText(s);
+    }
+
+    /**
+     * changes the screen scene
+     * @param mainStage where the schene must be loaded
+     * @throws IOException
+     */
+    void changeScene(Stage mainStage) throws IOException{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/match.fxml"));
+        Parent match = loader.load();
+
+        MatchController matchController = loader.getController();
+        matchController.setViewGUI(viewGUI);
+        viewGUI.setMatchController(matchController);
+        matchController.init();
+
+        //TODO da completare (far partire i timer)
+
+        Scene startScene;
+        startScene = new Scene(match, Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
+        mainStage.setScene(startScene);
+        mainStage.setMaximized(true);
+        mainStage.setFullScreen(true);
+        mainStage.show();
+        match.requestFocus();
+    }
+
+
 }
